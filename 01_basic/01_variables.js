882beb638d1522432ffb1ebbd764840b9909fc04
saifizaid63-oss <saifizaid63@gmail.com>
2026-02-20 09:10:58 +0000

    variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 1144553
+let accountEmail = "saifizaid63@gmail.com"
+var accountPassword ="123456"
+accountCity = "Bulandshahr" 
+// accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "dhbbdh.com"
+accountPassword = "2356252"
+accountCity = "sikandrabad"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountCity,accountPassword])
