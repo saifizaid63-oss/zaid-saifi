9da62e3d1eb686b769c2252c99fc43f02b60b429
saifizaid63-oss <saifizaid63@gmail.com>
2026-02-20 09:03:58 +0000

    datatypess

---
+++ 01_basic/02_database.js
@@ -0,0 +1,34 @@
+"use strict"; // treat all JS code are newer version 
+
+// alert (3+3) // we are using  nodejs, not browser
+
+console.log(3+
+    
+    3); // code readabiltiy should be high 
+
+
+
+console.log("zaid saifi");
+let name = "zaid saifi"
+let age = 18
+let isLoggedIn = false 
+
+
+//number => 2 to power 53
+//bigint 
+
+// string => ""
+
+// boolean =>true or false  // yes or no 
+// null => standalone value 
+// undefiend => 
+// symbole => unique 
+
+
+
+// object 
+
+console.log(typeof undefined ); // undefined
+console.log(typeof null);// object
+
+
