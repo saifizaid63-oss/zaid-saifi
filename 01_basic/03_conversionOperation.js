2909aa5fac1699c74b1700a8d2df168325640cbd
saifizaid63-oss <saifizaid63@gmail.com>
2026-02-21 09:43:30 +0000

    Operation

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "zaid saifi"
 
-console.log(typeof score );
-console.log(typeof (score) );
+// console.log(typeof score );
+// console.log(typeof (score) );
 
 let valueInNumber = Number (score)
-console.log(typeof valueInNumber );
-console.log(valueInNumber);
+// console.log(typeof valueInNumber );
+// console.log(valueInNumber);
 
 
 // "33"=>33
@@ -15,7 +15,7 @@ console.log(valueInNumber);
 let isLoggedIn = "zaid saifi"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 
 // 1 => true ; 0=> false 
@@ -25,8 +25,54 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33 
 
 let stringNumber = String(someNumber ) 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+//****************************************  Operation  */
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2)
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " zaid saifi"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+ "2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(3+4*5%3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+// console.log(num1 ,num2, num3);
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
 
 
 
