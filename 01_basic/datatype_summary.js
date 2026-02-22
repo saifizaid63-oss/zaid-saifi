5ca8a8fd34f6915e7646ca4c5ab39d01b719a8eb
saifizaid63-oss <saifizaid63@gmail.com>
2026-02-22 10:14:51 +0000

    datatype_summary

---
+++ 01_basic/datatype_summary.js
@@ -0,0 +1,37 @@
+// Primitive 
+
+// 7 types : String , Number , Boolean , null , undefined , symbol , BigInt 
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn =  false 
+const outsideTemp = null
+let userEamil;
+
+const id = Symbol ('123')
+const anotherId = Symbol ('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 80773228741234564526n
+
+
+
+// Reference type (Non primitive )
+
+//Array, objects , Function  
+
+const heros = ["batman ", "superman" , "spiderman"];
+let myObj = {
+    name : "zaid saifi",
+    'age' : 22,
+
+}
+
+const myFunction= function(){
+    console.log("Hello world");
+    
+}
+
+console.log(typeof myFunction);
