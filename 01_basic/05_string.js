01d1350f21b423e154eb708fba123140a841ef43
saifizaid63-oss <saifizaid63@gmail.com>
2026-02-23 09:58:49 +0000

    Strings

---
+++ 01_basic/05_string.js
@@ -0,0 +1,28 @@
+const name = "zaid saifi"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value");
+
+// console.log('Hello my name is ${name} and my repo count is ${repoCount}' );
+
+const gameName = new String('Pubg')
+
+// console.log(gameName[0]);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase);
+
+
+console.log(gameName.charAt(2));
+
+const newString = gameName.substring(0,3)
+console.log(newString);
+
+const anotherString = gameName.slice(2,3)
+console.log(anotherString);
+
+
+
+
+
+
+
