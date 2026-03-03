59eefcfd1fcd3b5c7d12c97f2869d5453f2a3bfc
saifizaid63-oss <saifizaid63@gmail.com>
2026-03-03 10:24:25 +0000

    control_flow_one.js

---
+++ 04_control_flow/one.js
@@ -0,0 +1,54 @@
+//  first " If "  statement 
+// const isUserloggedIn = true
+// const temerature = 41
+
+// if (temerature === 30 ){
+//     console.log("less then 50");
+// }else{
+//     console.log("temerature is greater then 50");
+// }
+// console.log("Execute");
+
+// const score = 200
+
+// if(score >100){
+//     let power = "fly"
+//     console.log(`User power : ${power}`);
+    
+// }
+// console.log(`User power : ${power}`);
+
+
+
+// const balance = 1000
+
+// // if(balance > 500) console.log("test"),console.log("test2");
+
+// if (balance < 500){
+//     console.log("less then 500 ");
+    
+// }else if (balance < 750){
+//     console.log("less then 750");
+    
+// }else if (balance < 900){
+//     console.log("less than 900");
+    
+// }else {
+//     console.log("less then 1200");
+    
+// }
+
+
+const isUserloggedIn = true 
+const debitCard = true 
+const LoggedInFromGoogle = false 
+const LoggedInFromEmail = true
+
+if (isUserloggedIn && debitCard && 2 == 3 ){
+    console.log("Allow to buy course");
+    
+}
+ if (LoggedInFromGoogle || LoggedInFromEmail){
+    console.log("User logged in ");
+    
+ }
