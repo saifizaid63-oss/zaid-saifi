b0eeba38470841870d706f64c44ba79425263b64
saifizaid63-oss <saifizaid63@gmail.com>
2026-03-08 10:03:15 +0000

    filter_loop

---
+++ 05_iteration/six.js
@@ -0,0 +1,30 @@
+// const coading = ["js", "ruby", "java", "paython ", "C++"]
+
+
+// const value = coading.forEach((item)=>{
+//     // console.log(item);
+//     return item
+// })
+
+// console.log(values);
+
+//filter basic *****************
+
+// const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNums.filter((num)=> {
+//    return  num > 4
+// })
+
+// const newNums = []
+
+// myNums.forEach((num) => {
+//     if (num > 4 ) {
+//         newNums.push (num)
+//     }
+    
+// })
+// console.log(newNums);
+
+
+num > 4
